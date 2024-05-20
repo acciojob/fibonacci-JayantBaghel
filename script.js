2bfbf85c051bdd76bd04496422d00cd47d134e3f
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-20 13:20:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let prev = 0, curr = 1;
+    for (let i = 2; i <= num; i++) {
+        [prev, curr] = [curr, prev + curr];
+    }
+    return curr;
 }
 
 module.exports = fibonacci;
